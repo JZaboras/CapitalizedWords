c63d59408974e78aa43e412aad39365fd82f1ba8
Jevgenijus Zaboras <jevgenijus.zaboras@pandaconnec.com>
2019-10-11 20:45:01 +0300

    Initial project with two functions created

---
+++ CapitalizedWords/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CapitalizedWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!".BeginsCapitalized());
+            Console.WriteLine("noncapitalized".BeginsCapitalized());
+            Console.WriteLine("Hello World!".IsCapitalizedSentence());
+            Console.WriteLine("My name is Frank".IsCapitalizedSentence());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CapitalizedWords/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapitalizedWords
+{
+    public static class Utilities
+    {
+        public static bool BeginsCapitalized(this string str)
+        {
+            string firstLetter = str.Substring(0, 1);
+            if (firstLetter == firstLetter.ToUpper()) return true;
+            return false;
+        }
+
+        public static bool IsCapitalizedSentence(this string str)
+        {
+            string[] words = str.Split(" ");
+            int wordsCount = words.Length;
+            foreach (string word in words)
+            {
+                if (word.BeginsCapitalized()) wordsCount--;
+            }
+            if(wordsCount == 0) return true;
+            return false;
+        }
+    }
+}
